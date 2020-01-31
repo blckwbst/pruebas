a8efb2dbea3fe735cab0b38d585fbf1be372396a
blckwbst <60514143+blckwbst@users.noreply.github.com>
2020-01-31 14:07:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    
-    <title>Pagina 1</title></head>
+  <head></head>
   <body>
     <h1>Hello, world!</h1>
     <div class="alert alert-primary" role="alert">
