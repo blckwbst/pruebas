bb82b2386b344755f4e8deb80e8bf8b529daa82f
blckwbst <60514143+blckwbst@users.noreply.github.com>
2020-01-31 13:44:01 -0600

    version 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <title>Pagina 1</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <div class="alert alert-primary" role="alert">
+  joto!
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+  </body>
+</html>
